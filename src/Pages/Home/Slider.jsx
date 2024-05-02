df2440c0a9df4bdf9e6dd24a2eada751ecc62bcb
rjspyk5 <rjspyk5@gmail.com>
2024-05-02 12:26:38 +0600

    carasoul overlay set

--- src/Pages/Home/Slider.jsx
+++ src/Pages/Home/Slider.jsx
@@ -11,6 +11,22 @@ export const Slider = () => {
       <div className="carousel w-full">
         <div id="slide1" className="carousel-item relative w-full">
           <img src={one} className="w-full h-[400px] md:h-[600px] rounded-xl" />
+          <div className="absolute bg-gradient-to-r from-[#15151500] to-[#151515]  h-full w-full  text-white  gap-3  ">
+            <div className="max-w-[500px] pl-10 space-y-4 absolute bottom-28">
+              {" "}
+              <h1 className="font-bold text-5xl ">
+                Affordable Price For Car Servicing
+              </h1>
+              <p>
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="space-x-3">
+                <button className="btn btn-primary">Discover More</button>
+                <button className="btn btn-primary"> Latest Projects</button>
+              </div>
+            </div>
+          </div>
           <div className="absolute flex justify-end transform -translate-y-1/2 gap-3 left-5 right-5 bottom-3">
             <a href="#slide4" className="btn btn-circle">
               ❮
@@ -22,6 +38,22 @@ export const Slider = () => {
         </div>
         <div id="slide2" className="carousel-item relative w-full">
           <img src={two} className="w-full h-[400px] md:h-[600px] rounded-xl" />
+          <div className="absolute bg-gradient-to-r from-[#15151500] to-[#151515]  h-full w-full  text-white  gap-3  ">
+            <div className="max-w-[500px] pl-10 space-y-4 absolute bottom-28">
+              {" "}
+              <h1 className="font-bold text-5xl ">
+                Affordable Price For Car Servicing
+              </h1>
+              <p>
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="space-x-3">
+                <button className="btn btn-primary">Discover More</button>
+                <button className="btn btn-primary"> Latest Projects</button>
+              </div>
+            </div>
+          </div>
           <div className="absolute flex justify-end transform -translate-y-1/2 gap-3 left-5 right-5 bottom-3">
             <a href="#slide1" className="btn btn-circle">
               ❮
@@ -36,6 +68,22 @@ export const Slider = () => {
             src={three}
             className="w-full h-[400px] md:h-[600px] rounded-xl"
           />
+          <div className="absolute bg-gradient-to-r from-[#15151500] to-[#151515]  h-full w-full  text-white  gap-3  ">
+            <div className="max-w-[500px] pl-10 space-y-4 absolute bottom-28">
+              {" "}
+              <h1 className="font-bold text-5xl ">
+                Affordable Price For Car Servicing
+              </h1>
+              <p>
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="space-x-3">
+                <button className="btn btn-primary">Discover More</button>
+                <button className="btn btn-primary"> Latest Projects</button>
+              </div>
+            </div>
+          </div>
           <div className="absolute flex justify-end transform -translate-y-1/2 gap-3 left-5 right-5 bottom-3">
             <a href="#slide2" className="btn btn-circle">
               ❮
@@ -50,6 +98,22 @@ export const Slider = () => {
             src={four}
             className="w-full h-[400px] md:h-[600px] rounded-xl"
           />
+          <div className="absolute bg-gradient-to-r from-[#15151500] to-[#151515]  h-full w-full  text-white  gap-3  ">
+            <div className="max-w-[500px] pl-10 space-y-4 absolute bottom-28">
+              {" "}
+              <h1 className="font-bold text-5xl ">
+                Affordable Price For Car Servicing
+              </h1>
+              <p>
+                There are many variations of passages of available, but the
+                majority have suffered alteration in some form
+              </p>
+              <div className="space-x-3">
+                <button className="btn btn-primary">Discover More</button>
+                <button className="btn btn-primary"> Latest Projects</button>
+              </div>
+            </div>
+          </div>
           <div className="absolute flex justify-end transform -translate-y-1/2 gap-3 left-5 right-5 bottom-3">
             <a href="#slide3" className="btn btn-circle">
               ❮
