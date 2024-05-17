150c7ccf9def70bc627b406c15695ee2bfceab88
rjspyk5 <rjspyk5@gmail.com>
2024-05-17 23:49:34 +0600

    sweet alert added in delete button for confrim delete

--- src/Pages/MyBookings.jsx
+++ src/Pages/MyBookings.jsx
@@ -14,16 +14,30 @@ export const MyBookings = () => {
   }, [user?.email]);
 
   const handleDelete = (id) => {
-    const proceed = confirm("Are you sure?");
-    if (proceed) {
-      axiosSecure
-        .delete(`/book/${id}`)
-        .then((res) => {
-          const update = data.filter((el) => el._id !== id);
-          setdata(update);
-        })
-        .catch((err) => console.log(err));
-    }
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure
+          .delete(`/book/${id}`)
+          .then((res) => {
+            const update = data.filter((el) => el._id !== id);
+            setdata(update);
+          })
+          .catch((err) => console.log(err));
+        Swal.fire({
+          title: "Deleted!",
+          text: "Your file has been deleted.",
+          icon: "success",
+        });
+      }
+    });
   };
 
   const handleConfrim = (id) => {
